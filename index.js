67c615ad9792c41f563e00820db0adfcf38be916
jdmedlock <jdmedlock@gmail.com>
2023-06-30 09:48:53 -0500

    Initial repo creation [WIP]

---
+++ index.js
@@ -0,0 +1,25 @@
+import { Command } from 'commander'
+import * as dotenv from 'dotenv'
+//import extractDiscordUsers from './src/extractDiscordUsers.js'
+
+// Process a request to extract user accounts from the Chingu Discord server
+(async () => {
+  const program = new Command()
+  dotenv.config()
+  program 
+    .command('assign <voyage>')
+    .description('Assign Voyager role to Discord users signed up for next Voyage')
+    .action(async (source, options, command) => {
+      try {
+        if (command._name === 'assign') {
+          await assignVoyagerRole(voyage.toLowerCase())
+        }
+      }
+      catch (err) {
+        console.log(err)
+        process.exit(0)
+      }
+    })
+
+    program.parse(process.argv)
+  })()
---
+++ src/Airtable/getVoyagers.js
@@ -0,0 +1,32 @@
+import Airtable from 'airtable'
+
+// Retrieve Chingus who have signed up to particpate in the specified Voyage
+const getVoyagers = async (voyageName) => {
+  return new Promise(async (resolve, reject) => {
+    const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE)
+    const filter = '{Voyage} = \"' + voyageName + "\" "
+
+    base('Voyage Signups').select({ 
+      filterByFormula: filter,
+      view: 'Most Recent Voyage Signups' 
+    })
+    .firstPage((err, records) => {
+      if (err) { 
+        console.error('getVoyagers - filter: ', filter)
+        console.error(err) 
+        reject(err) 
+      }
+
+      // If the record is found return its id. Otherwise, return null if it's
+      // not found
+      for (let i = 0; i < records.length; ++i) {
+        if (records.length > 0) {
+          resolve(records[i].id)
+        }
+      }
+      resolve(null)
+    })
+  })
+}
+
+export { getVoyagers }
