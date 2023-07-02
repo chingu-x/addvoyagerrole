9a3dbc6fa8052a0fb1dd50082b7cbbd03f184b70
jdmedlock <jdmedlock@gmail.com>
2023-07-02 09:10:05 -0500

    Fix: Resolve async/await issue in getUsersWithVoyagerRole.js

--- src/Discord/getUsersWithVoyagerRole.js
+++ src/Discord/getUsersWithVoyagerRole.js
@@ -1,48 +1,58 @@
 import { Client, GatewayIntentBits } from 'discord.js'
 
+let membersWithVoyagerRole = []
+
 const getUsersWithVoyagerRole = async () => {
-  const client = new Client({
-      intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMembers,
-      ],
-    })
-  const login = await client.login(process.env.DISCORD_TOKEN)
+  return new Promise(async (resolve, reject) => {
+    const client = new Client({
+        intents: [
+          GatewayIntentBits.Guilds,
+          GatewayIntentBits.GuildMembers,
+        ],
+      })
+    const login = await client.login(process.env.DISCORD_TOKEN)
 
-  try {
-    client.on('ready', async () => {
-      const guild = await client.guilds.fetch(process.env.GUILD_ID)
-      process.env.MODE.toUpperCase() === 'TEST' && 
-        console.log('...getVoyagers - guild: ', guild.name)
-
-      const members = await guild.members.fetch()
-
-      const voyagerRole = guild.roles.cache.get('1104544072347160578')
-      process.env.MODE.toUpperCase() === 'TEST' && 
-        console.log('...getVoyagers - Voyage role: ', voyagerRole.name) 
-
-      const voyagers = guild.roles.cache.get(voyagerRole.id).members.map(m => m)
-      process.env.MODE.toUpperCase() === 'TEST' && 
-        console.log('...getVoyagers - members with Voyage role: ') 
-
-      let voyagerCount = 0
-      for (const voyager of voyagers) {
-
-        voyagerCount = ++voyagerCount
-        const userName = voyager.user.username
-        const userId = voyager.user.id
+    try {
+      client.on('ready', async () => {
+        // Retrieve the guild object for Chingu's Discord server
+        const guild = await client.guilds.fetch(process.env.GUILD_ID)
         process.env.MODE.toUpperCase() === 'TEST' && 
-          console.log(`...Voyager - id: ${ userId } name: ${ userName }`) 
-      }
-    })
-  }
-  catch(err) {
-    console.log('='.repeat(30))
-    console.log('Error retrieving users with the Voyager role:')
-    console.log(err)
-    await client.destroy() // Terminate this Discord bot
-  }
+          console.log('...getUsersWithVoyagerRole - guild: ', guild.name)
 
+        await guild.roles.fetch() // Prime the DiscordJS cache
+        await guild.members.fetch() // Prime the DiscordJS cache
+
+        // Retrieve the Role object for the Voyager role
+        const voyagerRole = guild.roles.cache.find(role => role.name === "Voyager")
+        process.env.MODE.toUpperCase() === 'TEST' && 
+          console.log('...getUsersWithVoyagerRole - Voyage role: ', voyagerRole.name) 
+
+        // Retrieve the members who have the Voyager role
+        const voyagers = guild.roles.cache.find(role => role.id === voyagerRole.id)
+            .members.map(member => member)
+
+        // Create an array containing the members with this role
+        for (const voyager of voyagers) {
+          membersWithVoyagerRole.push({
+            id: voyager.user.id, 
+            name: voyager.user.username
+          })
+        }
+        
+        process.env.MODE.toUpperCase() === 'TEST' && 
+          console.log(`...membersWithVoyagerRole: `, membersWithVoyagerRole) 
+        resolve(membersWithVoyagerRole)
+      })
+
+    }
+    catch(err) {
+      console.log('='.repeat(30))
+      console.log('Error retrieving users with the Voyager role:')
+      console.log(err)
+      client.destroy() // Terminate this Discord bot
+      reject(null)
+    }
+  })
 }
 
 export default getUsersWithVoyagerRole
